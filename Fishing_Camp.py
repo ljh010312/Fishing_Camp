5b9251124dcddec62774884468261b28baa68891
Lee9747 <leejunho32@naver.com>
2024-05-18 21:59:07 +0900

    노트북3 그래프 화면 배치

--- Fishing_Camp.py
+++ Fishing_Camp.py
@@ -50,10 +50,7 @@ class MainGUI:
         Label(self.window, text='Fishing Camp', fg='black', font='helvetica 20').place(x=50, y=50)
 
         # 시군 선택 콤보박스 생성
-        self.selected_gu = StringVar()
-        self.selected_gu.set("가평군")  # 초기값 설정
-        self.gu_options = ['가평군', '고양시', '광명시', '광주시', '구리시', '군포시', '김포시', '남양주시', '부천시', '수원시', '시흥시', '안산시', '안성시', '안양시',
-                      '양주시', '양평군', '여주시', '연천군', '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시', '화성시']
+
         self.gu_combo = tkinter.ttk.Combobox(frame1, textvariable=self.selected_gu, values=list(self.gu_options))
         self.gu_combo.place(x=50, y=150)
 
@@ -95,21 +92,38 @@ class MainGUI:
                                f"위치: \n"
                                f"날씨: ")
 
+    def setNoteThree(self):
+        frame3 = Frame(self.window)
+        self.notebook.add(frame3, text='면적 그래프')
+        Label(frame3, text='면적 그래프', fg='black', font=('Consolas', 30)).pack()
+
+        # 시군 콤보 박스 생성
+        self.gu_combo = tkinter.ttk.Combobox(frame3, textvariable=self.selected_gu, values=list(self.gu_options))
+        self.gu_combo.pack()
+
+        # 그래프 canvas 생성
+        self.areaCanvas = Canvas(frame3, width=700, height=400)
+        self.areaCanvas.pack()
+
+        self.areaCanvas.create_rectangle(2,2, 700, 400)
+
+
+
     def __init__(self):
         self.window = Tk()
         self.window.title('Fishing Camp')
+        self.selected_gu = StringVar()
+        self.selected_gu.set("가평군")  # 초기값 설정
+        self.gu_options = ['가평군', '고양시', '광명시', '광주시', '구리시', '군포시', '김포시', '남양주시', '부천시', '수원시', '시흥시', '안산시', '안성시',
+                           '안양시',
+                           '양주시', '양평군', '여주시', '연천군', '오산시', '용인시', '의왕시', '의정부시', '이천시', '파주시', '평택시', '포천시', '하남시',
+                           '화성시']
         self.notebook = tkinter.ttk.Notebook(self.window, width=800, height=600)
         self.notebook.pack()
 
         self.setNoteOne()
         self.setNoteTwo()
-
-
-
-
-        frame3 = Frame(self.window)
-        self.notebook.add(frame3, text='면적 그래프')
-        Label(frame3, text='페이지3의 내용', fg='orange', font='helvetica 48').pack()
+        self.setNoteThree()
 
         self.window.mainloop()
 
